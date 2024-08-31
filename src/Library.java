68873536ceb99488de00320dd27d9f9c25e05161
AiD4HN <aganapin9@gmail.com>
2024-08-31 14:55:51 +0800

    feat: updated "Book" and "Library" class, removed unnes files

---
+++ src/Library.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+
+public class Library
+{
+
+    ArrayList<Book> libraryList = new ArrayList<>();
+
+    public void onInsertAnywhere(Book book)
+    {
+        libraryList.add(book);
+    }
+
+    public void onInsertAtSpecificIndex(int index, Book book)
+    {
+
+        libraryList.add(index, book);
+    }
+
+    public void onRemoveABookByIndex(int index)
+    {
+        libraryList.remove(index);
+    }
+
+}
