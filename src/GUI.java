85a882c0201d18ff30cbc855a930d57cb4c2aa91
Brian <brianpasco1206@gmail.com>
2024-08-31 15:02:23 +0800

    feat(interface): update graphical user interface

---
+++ src/GUI.java
@@ -0,0 +1,67 @@
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+
+public class GUI extends JFrame
+{
+
+    private JPanel LibraryPanel;
+    private JTable LibraryTable;
+    private JButton AddBooksBtn;
+    private JButton RemoveBooksBtn;
+    private JButton GetLibrarySizeBtn;
+    private JButton FindBooksBtn;
+    private JButton CheckIfEmptyBtn;
+    private DefaultTableModel MainTableModel;
+
+    Library library = new Library();
+
+    public void initialize()
+    {
+        initTable();
+        initActions();
+        initInterface();
+    }
+
+    private void initTable()
+    {
+        final String[] LibraryTableColumns = {
+                "Title", "Author", "Genre", "Publication Year"
+        };
+
+        MainTableModel = (DefaultTableModel) LibraryTable.getModel();
+
+        for (String LibraryTableColumn : LibraryTableColumns) {
+            MainTableModel.addColumn(LibraryTableColumn);
+        }
+
+        refreshTable();
+    }
+
+    private void refreshTable()
+    {
+
+        MainTableModel.setRowCount(0);
+
+        for (int i = 0; i < library.libraryList.size(); i++)
+        {
+            MainTableModel.addRow(library.libraryList.get(i).getDetails());
+        }
+    }
+
+    private void initInterface()
+    {
+        setContentPane(LibraryPanel);
+        setTitle("Library Management System");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(576, 384);
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+
+    public static void main(String[] args)
+    {
+        GUI gui = new GUI();
+        gui.initialize();
+    }
+
+}
