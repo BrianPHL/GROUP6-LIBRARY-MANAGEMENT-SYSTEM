8ecfbd741759d7739aa4f83456122b284cd67416
Gerald Elli Ramos <ramosgerald2003@gmail.com>
2024-09-23 23:31:01 +0800

    feat: update History.java

--- src/History.java
+++ src/History.java
@@ -13,6 +13,36 @@ public class History
         this.gui = gui;
         this.library = library;
     }
+    public void undo()
+    {
 
+        if (!historyStack.isEmpty())
+        {
+            Action lastAction = historyStack.pop();
+
+            if (lastAction.getAction().equals("add"))
+            {
+                int index = Integer.parseInt(lastAction.getBook().getDetails()[0]);
+                library.libraryList.remove(index);
+                JOptionPane.showMessageDialog(null, "Successfully undo recent action: Add \"" + lastAction.getBook().getDetails()[1] + "\" book.");
+                gui.refreshTable();
+            }
+            else if (lastAction.getAction().equals("remove"))
+            {
+                Book book = lastAction.getBook();
+                library.libraryList.add(book);
+                JOptionPane.showMessageDialog(null, "Successfully undo recent action: Remove \"" + book.getDetails()[1] + "\" book.");
+                gui.refreshTable();
+            }
+        }
+        else
+        {
+            JOptionPane.showMessageDialog(
+                    null,
+                    "No actions to undo at this time."
+            );
+        }
+
+    }
 }
   
