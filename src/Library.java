1943af7f94ba902f61d41b47b2b4a6c82245b35a
Brian <brianpasco1206@gmail.com>
2024-09-23 23:22:20 +0800

    feat(global): update Library.java

--- src/Library.java
+++ src/Library.java
@@ -3,19 +3,16 @@ import java.util.ArrayList;
 public class Library
 {
 
+    private static Library instance;
+
     ArrayList<Book> libraryList = new ArrayList<>();
 
-    public void onInsertAnywhere(Book book)
-    {
-        libraryList.add(book);
-    }
-
-    public void onInsertAtSpecificIndex(int index, Book book)
+    public void onInsert(int index, Book book)
     {
         libraryList.add(index, book);
     }
 
-    public void onRemoveABookByIndex(int index)
+    public void onRemove(int index)
     {
         libraryList.remove(index);
     }
@@ -26,4 +23,33 @@ public class Library
     public boolean isEmpty() {
         return libraryList.isEmpty();
     }
+
+    public void bubbleSortByTitle()
+    {
+        int libraryListSize = libraryList.size();
+        boolean isSwapped;
+
+        for (int i = 0; i < libraryListSize-1; i++)
+        {
+            isSwapped = false;
+
+            for (int j = 0; j < (libraryListSize - i - 1); j++)
+            {
+
+                if (libraryList.get(j).getDetails()[1].compareTo(libraryList.get(j+1).getDetails()[1]) > 0)
+                {
+                    Book temp = libraryList.get(j);
+                    libraryList.set(j, libraryList.get(j + 1));
+                    libraryList.set(j + 1, temp);
+                    isSwapped = true;
+                }
+
+            }
+
+            if (!isSwapped) break;
+
+        }
+
+    }
+
 }
